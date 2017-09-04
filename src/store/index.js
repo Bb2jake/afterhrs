db6ba1451ce38aec8578e7655378648eced13eaf
Matt Young <matthewdyoung@att.net>
2017-09-04 14:49:11 -0600

    Tweaked search function.

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,22 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import $ from 'jquery'
+
+
 Vue.use(Vuex)
+
+
 const state = {
     results: []
 }
+
+$.ajaxSetup({
+    crossDomain: true,
+    xhrFields: {
+        withCredentials: true
+    }
+})
+
 const mutations = {
     setUsers(state, data) {
         state.results = data
@@ -11,7 +24,7 @@ const mutations = {
 }
 const actions = {
     search({ commit }, { data }) {
-        Vue.http.get("https://addb.absolutdrinks.com/drinks/absolut-cosmopolitan/?apiKey=0ff6ee2b1fef44fab626292fb8348cb5")
+        fetch("https://addb.absolutdrinks.com/drinks/absolut-cosmopolitan/?apiKey=0ff6ee2b1fef44fab626292fb8348cb5")
             .then((response) => {
                 commit("setUsers", response.body);
             })
