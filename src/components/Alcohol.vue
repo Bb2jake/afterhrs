27bc8386d8270c1e0ae832204b9e1250ec65adbb
Matt Young <matthewdyoung@att.net>
2017-09-04 17:12:21 -0600

    Fixed the majority of the search function for drinks.

--- src/components/Alcohol.vue
+++ src/components/Alcohol.vue
@@ -2,18 +2,29 @@
   <div class="container">
     <div class="row text-center">
       <h3>Results: {{ $store.state.results }} </h3>
-      <button class="btn btn-success" @click="search">Search</button>
+      <form @submit.prevent="search">
+        <input type="text" placeholder="Search for a drink..." v-model="query">
+        <button class="btn btn-success" type="submit">Search</button>
+      </form>
     </div>
   </div>
 </template>
 <script>
   import { mapGetters, mapActions } from 'vuex'
+
   export default {
-    name: 'hello',
+    name: 'alcohol',
+    data(){
+      return{
+        query: ''
+      }
+    },
     computed: mapGetters([
     ]),
-    methods: mapActions([
-      'search'
-    ])
+    methods: {
+      search: function () {
+        this.store.dispatch('search', this.query)
+      }
+    }
   }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -10,25 +10,22 @@ const state = {
     results: []
 }
 
-$.ajaxSetup({
-    crossDomain: true,
-    xhrFields: {
-        withCredentials: true
-    }
-})
-
 const mutations = {
-    setUsers(state, data) {
-        state.results = data
+    setResults(state, results) {
+        state.results = JSON.parse(results)
     }
 }
 const actions = {
-    search({ commit }, { data }) {
-        fetch("https://addb.absolutdrinks.com/drinks/absolut-cosmopolitan/?apiKey=0ff6ee2b1fef44fab626292fb8348cb5")
-            .then((response) => {
-                commit("setUsers", response.body);
-            })
-            .catch((error => {
+    search({ commit, dispatch }, query ) {
+        var getterUrl = '//bcw-getter.herokuapp.com/?url=';
+        var apiBaseUrl = 'https://addb.absolutdrinks.com/drinks/';
+        var apiKey = '/?apiKey=0ff6ee2b1fef44fab626292fb8348cb5';
+        var appId = '?appId=14689';
+        var requestString = getterUrl + encodeURIComponent(apiBaseUrl + query + apiKey);
+        $.get(requestString).then(data => {
+            commit('setResults', data)
+            console.log(state.results)
+        }).catch((error => {
                 console.log(error.statusText)
             }))
     }
